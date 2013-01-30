d83f922a08738bf18287fcb5e8bb563e79e7bbc4
jkibele <jkibele@gmail.com>
2013-01-30 18:20:30 +1300

    Slow progress on new qt gui

--- benthic_photo_survey.py
+++ benthic_photo_survey.py
@@ -1,4 +1,5 @@
-import sys
+import sys, os
+import glob
 #from photo_tagging import *
 from PyQt4 import QtCore
 from PyQt4.QtGui import QApplication, QMainWindow, QFileDialog
@@ -9,12 +10,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)                                         
         self.setupUi(self)
-        self.actionLoad_Image_Directory.triggered.connect(self.loadPhotoDirectory)
+        self.picPaths = []
+        self.currentPhotoIndex = 0
+        self.actionLoad_Photo_Directory.triggered.connect(self.loadPhotoDirectory)
 
     def loadPhotoDirectory(self):
-        photo_dir = QFileDialog.getExistingDirectory(self, 'Open Photo Directory', directory='~')
-        print photo_dir
-        self.statusBar().showMessage('File menu: New selected', 8000)
+        photo_dir = str( QFileDialog.getExistingDirectory(self, 'Open Photo Directory', directory='data/images') )
+        self.picPaths = glob.glob(photo_dir + os.path.sep + "*.JPG") + glob.glob(photo_dir + os.path.sep + "*.jpg")
+        self.picPaths.sort()
+        
+        self.statusBar().showMessage('Photo Directory Set to: %s', 8000) % photo_dir
+        
+    def nextPhoto(self):
+        
                 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
