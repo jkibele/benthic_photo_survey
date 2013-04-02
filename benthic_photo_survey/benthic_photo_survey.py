32c394dcd21cbe230a888b64c7264c0a5b0685b7
jkibele <jkibele@gmail.com>
2013-04-02 16:28:37 +1300

    Trying to make distribution work properly. Not having the best of luck.

--- benthic_photo_survey/benthic_photo_survey.py
+++ benthic_photo_survey/benthic_photo_survey.py
@@ -1,3 +1,5 @@
+#! /usr/bin python
+
 import sys, os
 import glob
 from depth_temp_log_io import *
--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@
 from distutils.core import setup
 
 setup(name='BenthicPhotoSurvey',
-      version='0.3.3',
+      version='0.3.8',
       description='Benthic Photo Survey',
       author='Jared Kibele',
       author_email='jkibele@gmail.com',
       url='https://bitbucket.org/jkibele/benthic_photo_survey',
-      packages=['benthic_photo_survey'],
+      packages=['benthic_photo_survey','docs','test_data'],
+      include_package_data = True,
+      scripts=['benthic_photo_survey/benthic_photo_survey.py'],
+#      data_files=[('docs',['docs/build/html/*']),
+#                  ('test_data/gps', ['test_data/gps/*.gpx']),
+#                  ('test_data/images', ['test_data/images/great_barrier_island/*.JPG']),
+#                  ('test_data/sensus', ['test_data/sensus/*.csv'])]
      )
 
