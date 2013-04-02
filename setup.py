be9ae8dab86e8145b6d8a4e395c00695ea2bdf5c
jkibele <jkibele@gmail.com>
2013-04-02 17:44:56 +1300

    Struggling with distribution still.

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@
 from distutils.core import setup
 
 setup(name='BenthicPhotoSurvey',
-      version='0.3.8',
+      version='0.3.9',
       description='Benthic Photo Survey',
       author='Jared Kibele',
       author_email='jkibele@gmail.com',
       url='https://bitbucket.org/jkibele/benthic_photo_survey',
       packages=['benthic_photo_survey','docs','test_data'],
-      include_package_data = True,
+      package_data={'test_data': ['test_data/gps/gb*.gpx',
+                                  'test_data/sensus/gb*.csv',
+                                  'test_data/images/great_barrier_island/*.JPG'],
+                    'docs': ['build/html/*',
+                             'docs/build/html/_images/*',
+                             'docs/build/html/_modules/*',
+                             'docs/build/html/_sources/*',
+                             'docs/build/html/_static/*',]},
       scripts=['benthic_photo_survey/benthic_photo_survey.py'],
-#      data_files=[('docs',['docs/build/html/*']),
-#                  ('test_data/gps', ['test_data/gps/*.gpx']),
-#                  ('test_data/images', ['test_data/images/great_barrier_island/*.JPG']),
-#                  ('test_data/sensus', ['test_data/sensus/*.csv'])]
+      
      )
 
