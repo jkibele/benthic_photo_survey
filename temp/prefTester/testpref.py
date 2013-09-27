a222c08fcca5df246ecf8e9fe0ef9eca54c66b10
jkibele <jkibele@gmail.com>
2013-09-27 17:45:51 +1200

    More progress on custom preferences thing. Man, what a pain in the arse.

--- temp/prefTester/testpref.py
+++ temp/prefTester/testpref.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-import sys
+import sys, json
 from PyQt4 import QtCore
 from PyQt4.QtCore import QSettings, QModelIndex
 from PyQt4.QtGui import QApplication, QMainWindow, QFileDialog, QPixmap, \
-    QMessageBox, QDialog, QColor, QColorDialog
+    QMessageBox, QDialog, QColor, QColorDialog, QTableWidgetItem
 from ui_mainwindow import Ui_MainWindow
 from ui_testpref import Ui_Dialog
 
-class prefRow(object):
+class PrefRow(object):
     def __init__(self,name=None,code=None,color=None):
         """
         name and color will be strings. code will be an int.
@@ -16,39 +16,115 @@ class prefRow(object):
         self.code = code
         self.color = color
         
+    def __repr__(self):
+        return str( self.toList() )
+        
     def toList(self):
         return [self.name,self.code,self.color]
+        
+    def fromList(self,inlist):
+        self.name = inlist[0]
+        self.code = inlist[1]
+        self.color = inlist[2]
         
     @property
     def q_color(self):
         return QColor( self.color )
         
-class prefArray(object):
+    def addToTableWidget(self,widget,rownum):
+        itemlist = [self.code,self.name,self.color]
+        for i,thing in enumerate(itemlist):
+            item = QTableWidgetItem( thing )
+            if i==len(itemlist)-1:
+                item.setBackgroundColor( QColor(self.color) )
+            widget.setItem(rownum,i,item)
+        
+class PrefArray(object):
     def __init__(self,rowList=None):
-        self.rows = rowList
+        if rowList:        
+            self.rows = rowList
+        else:
+            self.rows = []
         
     def __repr__(self):
-        return self.rows
+        return str( self.rows )
+        
+    def addrow(self, pref_row):
+        self.rows.append(pref_row)
+        
+    def addToTableWidget(self,widget):
+        for i,row in enumerate(self.rows):
+            row.addToTableWidget(widget,i)
+        
+    def saveToSettings(self,settings,setting_tag):
+        json_str = json.dumps( self.toList() )
+        settings.setValue( setting_tag, json_str )
+        
+    def loadFromSettings(self,settings,setting_tag):
+        json_str = str( settings.value(setting_tag,'[[null, null, null]]').toString() )
+        self.fromJson(json_str)
+        
+    def fromJson(self,json_str):
+        """
+        Take a json string and load it into a list of rows. Then load that into
+        PrefRows and load those into a PrefArray.
+        """
+        # turn the json into a list of lists (rows)
+        print json_str
+        jlist = json.loads(json_str)
+        self.clear()
+        for row in jlist:
+            pr = PrefRow()
+            pr.fromList( row )
+            self.addrow( pr )
+        
+    def clear(self):
+        """
+        Clear out all data.
+        """
+        self.rows = []
+    
+    def toList(self):
+        outlist = []
+        for r in self.rows:
+            outlist.append(r.toList())
+        return outlist
+        
+    @property
+    def rowCount(self):
+        return len(self.rows)
 
 class StartPrefs(QDialog, Ui_Dialog):
     def __init__(self,parent=None):
         QDialog.__init__(self,parent)
         self.setupUi(self)
-        self.settings = QSettings("jkibele","TestPref")
+        self.settings = QSettings("test","tpref2")
+        self.habPrefArray = PrefArray()
+        self.habPrefArray.loadFromSettings(self.settings,"tpref2")
+        # setup habitat table
+        self.habTableWidget.setColumnCount(3)
+        self.habTableWidget.setRowCount(self.habPrefArray.rowCount)
+        self.habPrefArray.addToTableWidget(self.habTableWidget)
+        self.habTableWidget.setHorizontalHeaderLabels(["Code","Habitat","Color"])
     
-    def addRow(self):
+    def addHabRow(self):
+        if self.habLineEdit.text():
+            newRow = PrefRow(name=self.habLineEdit.text())
+            newRow.addToTableWidget(self.habTableWidget)
+    
+    def removeHabRow(self):
         pass
     
-    def removeRow(self):
+    def moveHabUp(self):
         pass
     
-    def moveUp(self):
+    def moveHabDown(self):
         pass
     
-    def moveDown(self):
+    def changeHabColor(self):
         pass
     
-    def changeColor(self):
+    def habHelp(self):
         pass
 
 class MainWindow(QMainWindow, Ui_MainWindow):
